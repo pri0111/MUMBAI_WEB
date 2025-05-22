298d1cec2b64bc35f755e2eab633c1681bdf0fce
PRIYA MANNA <priyamanna0111@gmail.com>
2025-05-22 19:42:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const githubLinks = {
+    "1990": " https://pri0111.github.io/MUMBAI_1990/",
+    "2000": "https://pri0111.github.io/MUMBAI_2000/",
+    "2010": " https://pri0111.github.io/MUMBAI_2010/",
+    "2020": "https://pri0111.github.io/MUMBAI_2020/"
+  };
+
+const services = [
+  "Provisional Service of Shrub","Regulating Service of Shrub", "Supporting Service of Shrub","Cultural Service of Shrub",
+  "Provisional Service of Waterbody","Regulating Service of Waterbody","Supporting Service of Waterbody","Cultural Service of Waterbody",
+  "Provisional Service of Vegetation", "Regulating Service of Vegetation", "Supporting Service of Vegetation", "Cultural Service of Vegetation",
+  ];
+ 
